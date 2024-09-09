a1e22fcb56b20405fe1e76d88c3df12595cf1efa
Samuel Fuentes <safuca2005@gmail.com>
2024-09-09 13:39:10 -0400

    Updated URLs for deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createTheme, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = "https://latambioenergy101.azurewebsites.net/api/v1";
 axios.defaults.withCredentials = true;
-const theme = createTheme({
-  typography: {
-    fontFamily: "Inter",
-    allVariants: { color: "white" },
-  },
-});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
