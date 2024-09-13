b708ee3ba4d26490a42508621c6f79b9a6b996f2
Samuel Fuentes <safuca2005@gmail.com>
2024-09-13 14:30:40 -0400

    Fixed vite frontend for vercel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.baseURL = "https://lb-101-form-app-h6ssz9pbn-solidaritytech.vercel.app/api/v1";
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
