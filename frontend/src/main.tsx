dad7baa071314fa03e9b8b7d86550f4f0858951b
Samuel Fuentes <safuca2005@gmail.com>
2024-09-13 15:00:45 -0400

    Fixed URLs 2.0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://lb-101-form-app-hzq370391-solidaritytech.vercel.app/api/v1";
+axios.defaults.baseURL = "https://lb-101-form-app-api.vercel.app/api/v1";
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
